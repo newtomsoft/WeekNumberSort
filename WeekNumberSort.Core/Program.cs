c2c096fe9f1af472088e005e7385b30f6bdf3ff8
Thomas Vuille <thomas@newtomsoft.fr>
2021-12-27 12:55:18 +0100

    dotnet6

--- WeekNumberSort.Core/Program.cs
+++ WeekNumberSort.Core/Program.cs
@@ -3,48 +3,41 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 
-namespace WeekNumberSort.Core
+
+var cultureInfo = new CultureInfo("fr-FR");
+var calendar = cultureInfo.Calendar;
+var calendarWeekRule = cultureInfo.DateTimeFormat.CalendarWeekRule;
+var firstDayOfWeek = cultureInfo.DateTimeFormat.FirstDayOfWeek;
+var sourceDirectory = args.Length == 0 ? "." : args[0];
+var movedFilesNumber = 0;
+var failedMovedFilesNumber = 0;
+var availablePatternExtensions = new List<string>() { "*.jpg", "*.png" };
+foreach (var patternExtension in availablePatternExtensions)
 {
-    class Program
+    foreach (var fullFileName in Directory.EnumerateFiles(sourceDirectory, patternExtension))
     {
-        static void Main(string[] args)
+        var createdDate = File.GetCreationTime(fullFileName);
+        var modifiedDate = File.GetLastWriteTime(fullFileName);
+        var date = createdDate < modifiedDate ? createdDate : modifiedDate;
+        var weekOfYear = calendar.GetWeekOfYear(date, calendarWeekRule, firstDayOfWeek);
+        var formattedWeekOfYear = weekOfYear > 9 ? weekOfYear.ToString() : "0" + weekOfYear;
+        var year = date.Year;
+        var oldDirectoryName = Path.GetDirectoryName(fullFileName);
+        var fileName = Path.GetFileName(fullFileName);
+        var newDirectoryName = Path.Combine(oldDirectoryName, year + "-" + formattedWeekOfYear);
+        var newFileName = Path.Combine(newDirectoryName, fileName);
+        Directory.CreateDirectory(newDirectoryName);
+        try
         {
-            var cultureInfo = new CultureInfo("fr-FR");
-            var calendar = cultureInfo.Calendar;
-            var calendarWeekRule = cultureInfo.DateTimeFormat.CalendarWeekRule;
-            var firstDayOfWeek = cultureInfo.DateTimeFormat.FirstDayOfWeek;
-            var sourceDirectory = args.Length == 0 ? "." : args[0];
-            var movedFilesNumber = 0;
-            var failedMovedFilesNumber = 0;
-            var availablePatternExtensions = new List<string>() {"*.jpg", "*.png" };
-            foreach (var patternExtension in availablePatternExtensions)
-            {
-                foreach (var fullFileName in Directory.EnumerateFiles(sourceDirectory, patternExtension))
-                {
-                    var createdDate = File.GetCreationTime(fullFileName);
-                    var modifiedDate = File.GetLastWriteTime(fullFileName);
-                    var date = createdDate < modifiedDate ? createdDate : modifiedDate;
-                    var weekOfYear = calendar.GetWeekOfYear(date, calendarWeekRule, firstDayOfWeek);
-                    string formattedWeekOfYear = weekOfYear > 9 ? weekOfYear.ToString() : "0" + weekOfYear;
-                    var year = date.Year;
-                    var oldDirectoryName = Path.GetDirectoryName(fullFileName);
-                    var fileName = Path.GetFileName(fullFileName);
-                    var newDirectoryName = Path.Combine(oldDirectoryName, year + "-" + formattedWeekOfYear);
-                    var newFileName = Path.Combine(newDirectoryName, fileName);
-                    Directory.CreateDirectory(newDirectoryName);
-                    try
-                    {
-                        Directory.Move(fullFileName, newFileName);
-                        movedFilesNumber++;
-                    }
-                    catch
-                    {
-                        failedMovedFilesNumber++;
-                    }
-                }
-            }
-            Console.WriteLine(movedFilesNumber + " files moved");
-            Console.WriteLine(failedMovedFilesNumber + " files not moved");
+            Directory.Move(fullFileName, newFileName);
+            movedFilesNumber++;
+        }
+        catch
+        {
+            failedMovedFilesNumber++;
         }
     }
 }
+Console.WriteLine(movedFilesNumber + " files moved");
+Console.WriteLine(failedMovedFilesNumber + " files not moved");
+
